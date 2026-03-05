894deee1c393fcc9e1812bddd6eb195f94493458
Jacob <jacob@switchtechnologies.net>
2026-03-05 16:31:22 +0800

    real-axis mirroring (works with identical results)

--- mandelbrot/src/serial/serial_2.c
+++ mandelbrot/src/serial/serial_2.c
@@ -15,17 +15,18 @@ int main() {
     short  green, blue;
     float  *x, *zi, *zj;
     float  ki, kj;
+    int    half = (N/2 + 1) * N;
 
 #ifdef FILE_IO
     FILE   *fp;
 #endif
 
-    zi = (float*)malloc(N*N/2 * sizeof(float));
-    zj = (float*)malloc(N*N/2 * sizeof(float));
-    x = (float*)malloc(N*N/2 * sizeof(float));
+    zi = (float*)malloc(half * sizeof(float));
+    zj = (float*)malloc(half * sizeof(float));
+    x = (float*)malloc(N*N * sizeof(float));
   
     // Initialise z
-    for (loop = 0; loop < N*N/2; loop++) {
+    for (loop = 0; loop < half; loop++) {
         i = loop % N;
         j = loop / N;
 
@@ -33,7 +34,7 @@ int main() {
         zj[loop] = (4.0 * (j - (float)N/2)) / N;
     }
 
-    for (loop = 0; loop < N*N/2; loop++) {
+    for (loop = 0; loop < half; loop++) {
         ki = zi[loop];
         kj = zj[loop];
 
@@ -47,6 +48,13 @@ int main() {
 	    x[loop] = log((float)k) / log((float)MAXITER);
     }
 
+    // Mirror rows 1..N/2-1 to rows N-1..N/2+1
+    for (j = 1; j < N/2; j++) {
+        for (i = 0; i < N; i++) {
+            x[(N - j) * N + i] = x[j * N + i];
+        }
+    }
+
 /* ----------------------------------------------------------------*/
   
 #ifdef FILE_IO
@@ -54,7 +62,7 @@ int main() {
     fp = fopen ("mandelbrot.ppm", "w");
     fprintf (fp, "P6\n%4d %4d\n255\n", N, N);
     
-    for (loop = 0; loop < N*N/2; loop++) { 
+    for (loop = 0; loop < N*N; loop++) { 
 	    if (x[loop] < 0.5) {
 	        green = (int)(2 * x[loop] * 255);
             fprintf(fp, "%c%c%c", 255 - green, green, 0);
@@ -62,18 +70,6 @@ int main() {
 	        blue = (int)(2 * x[loop] * 255 - 255);
             fprintf(fp, "%c%c%c", 0, 255 - blue, blue);
 	    }
-    }
-    for (j = N/2 - 1; j >= 0; j--) {
-        for (i = 0; i < N; i++) {
-            loop = j * N + i;
-            if (x[loop] < 0.5) {
-                green = (int)(2 * x[loop] * 255);
-                fprintf(fp, "%c%c%c", 255 - green, green, 0);
-            } else {
-                blue = (int)(2 * x[loop] * 255 - 255);
-                fprintf(fp, "%c%c%c", 0, 255 - blue, blue);
-            }
-        }
     }
     
     fclose(fp);
@@ -85,4 +81,3 @@ int main() {
     free(zj);
     free(x);
 }
-
