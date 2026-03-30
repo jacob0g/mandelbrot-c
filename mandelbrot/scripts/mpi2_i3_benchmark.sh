6204ec79753f2efd2484446cdb9bd8d3610bfdfe
Jacob <jacob@switchtechnologies.net>
2026-03-31 01:43:01 +0800

    Added benchmark scripts for mpi2 variants

---
+++ mandelbrot/scripts/mpi2_i3_benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash --login
+
+#SBATCH --account=courses0100
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:0
+
+echo "Benchmarking: mpi2_i3"
+srun -n 10 build/mpi_2_i3 5000
